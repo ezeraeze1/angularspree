b7d4e61b8cdda91acb8f3e4155c5315685d59248
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-08-02 22:14:43 +0530

    removed product cache

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -5,9 +5,8 @@ import { Taxonomy } from './../models/taxonomy';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
-import { map, tap } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import { TransferState, makeStateKey } from '@angular/platform-browser';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
@@ -21,7 +20,6 @@ export class ProductService {
     private http: HttpClient,
     private toastrService: ToastrService,
     private apiParser: JsonApiParserService,
-    private state: TransferState
   ) { }
   // tslint:disable-next-line:member-ordering
   success: any;
@@ -36,13 +34,6 @@ export class ProductService {
    * @memberof ProductService
    */
   getProduct(id: string): Observable<Product> {
-    const SSR_STATE_KEY = makeStateKey(`product-${id}`);
-    const ssrProduct = this.state.get(SSR_STATE_KEY, null as any);
-
-    if (ssrProduct) {
-      return of(ssrProduct);
-    }
-
     return this.http
       .get<{ data: CJsonApi }>(
         `api/v1/products/${id}?data_set=large&${+new Date()}`
@@ -50,7 +41,6 @@ export class ProductService {
       .pipe(
         map(resp => {
           const product = this.apiParser.parseSingleObj(resp.data) as Product;
-          this.state.set(SSR_STATE_KEY, product as any);
           return product;
         })
       );
