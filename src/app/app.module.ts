a979562df574d6305b70768173b135f4a9d3df60
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-08-02 10:15:55 +0530

    working on http caching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RouterModule } from '@angular/router';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {TransferHttpCacheModule} from '@nguniversal/common';
 
 // Components
 import { AppComponent } from './app.component';
@@ -61,6 +62,7 @@ import { ToastrModule } from 'ngx-toastr';
     EffectsModule.forRoot([]),
     BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'ng-spree' }),
+    TransferHttpCacheModule,
     FormsModule,
     HttpModule,
     HomeModule,
